d386e2a85cbb351da2266eabc8bb1fb5d2c6289d
AnnaGolosova <golosowa.anna@gmail.com>
2022-01-11 17:46:36 +0300

    Add project files.

---
+++ StringHelper/StringHelper.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StringHelper
+{
+    public static class StringHelper
+    {
+        public static List<string> FindMaxSequenceForEveryWord(string str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str");
+            }
+
+            var result = new List<string>();
+
+            // Delete all tabs
+            string withoutTabs = Regex.Replace(str, @"\s+", " ");
+
+            // Get all words
+            var words = withoutTabs.Split(' ').ToList();
+
+            // Find different characters for every word
+            int wordMaximum = 0;
+            string currentCharacter = default;
+            int currentMax = default;
+
+            // Running through all words
+            foreach (var word in words)
+            {
+
+                // Running through all characters
+                for (int j = 0; j < word.Length; j++)
+                {
+                    // Running for every character
+
+                    if ((j != 0) && (word[j] == word[j - 1]))
+                    {
+                        currentMax++;
+                    }
+                    else
+                    {
+                        if (currentMax > wordMaximum)
+                        {
+                            currentCharacter = word[j-1].ToString();
+                            wordMaximum = currentMax;
+                        }
+
+                        currentMax = 1;
+                    }
+                }
+                result.Add(currentCharacter.ToString() + " " + wordMaximum.ToString());
+
+                wordMaximum = 0;
+                currentMax = 0;
+            }
+            return result;
+        }
+    }
+}
---
+++ TheLongestSequence/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var str = "Carramba doom EeefeeTttstt";
+var result = StringHelper.StringHelper.FindMaxSequenceForEveryWord(str);
+
+foreach (var item in result)
+{
+    Console.WriteLine($"{item}");
+}
+
+Console.WriteLine();
